e252f3934cdff53581f5d3a0637a7d70e068e867
Fazil Amirli <amirli.fazil@student.atilim.edu.tr>
2021-10-28 16:20:06 +0400

    some calculator logic added

--- calculator project/script.js
+++ calculator project/script.js
@@ -11,5 +11,33 @@ updateDisplayValue();
 
 keys.addEventListener("click", function (e) {
   const element = e.target;
-  console.log(element);
+  if (!element.matches("button")) return;
+
+  if (element.classList.contains("operator")) {
+    console.log("operator", element.value);
+    return;
+  }
+
+  if (element.classList.contains("decimal")) {
+    //console.log("decimal", element.value);
+    inputDecimal(element.value);
+    updateDisplayValue();
+    return;
+  }
+  if (element.id === "clear-all") {
+    displayValue = "0";
+    updateDisplayValue();
+    return;
+  }
+
+  inputNumber(element.value);
+  updateDisplayValue();
 });
+
+function inputNumber(num) {
+  displayValue = displayValue === "0" ? num : displayValue + num;
+}
+
+function inputDecimal() {
+  if (!displayValue.includes(".")) displayValue += ".";
+}
