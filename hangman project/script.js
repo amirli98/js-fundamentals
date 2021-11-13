9de184faf1ada3a6392e358aa3686ab0f9156ca7
Fazil Amirli <amirli.fazil@student.atilim.edu.tr>
2021-11-14 00:08:05 +0300

    added game logic

--- hangman project/script.js
+++ hangman project/script.js
@@ -1,8 +1,17 @@
 const word_element = document.getElementById("word");
 
-const correctLetters = ["j", "a"];
-const wrong = [];
+const alphabet = Array.from(Array(26))
+  .map((e, i) => i + 65)
+  .map((y) => String.fromCharCode(y));
 
+const wrongLetters_element = document.getElementById("wrong-letters");
+
+let wrongLetters = [];
+let selectedWord = getRandomWord();
+let correctLetters = [];
+const popup = document.getElementById("popup-container");
+const playAgain = document.getElementById("play-again");
+const message_element = document.getElementById("success-message");
 function getRandomWord() {
   const words = [
     "javascript",
@@ -39,19 +48,59 @@ function getRandomWord() {
 }
 
 function displayWord() {
-  const selectedWord = getRandomWord();
   word_element.innerHTML = `
     ${selectedWord
       .split("")
       .map(
         (letter) => `
             <div class="letter">
-                ${letter}
+                ${correctLetters.includes(letter) ? letter : ""}
             </div>
         `
       )
       .join("")}
   `;
+  const w = word_element.innerText.replace(/\n/g, "");
+  if (w === selectedWord) {
+    message_element.innerText = "Congrats, you won!";
+    popup.style.display = "flex";
+  }
 }
 
+function updateWrongLetters() {
+  wrongLetters_element.innerHTML = `
+  ${wrongLetters.length > 0 ? "<h3>Wrong letters:</h3> <br>" : ""}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+}
+
+window.addEventListener("keydown", function (e) {
+  if (alphabet.includes(e.key.toUpperCase())) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        //TODO: You added this letter already!
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLetters();
+      } else {
+        //TODO: You added this letter before!
+      }
+    }
+  }
+});
+
 displayWord();
+playAgain.addEventListener("click", (e) => {
+  correctLetters = [];
+  wrongLetters = [];
+  popup.style.display = "none";
+  selectedWord = getRandomWord();
+  wrongLetters_element.innerHTML = "";
+  displayWord();
+});
